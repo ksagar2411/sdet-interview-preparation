67fead8939c046e1ebaeb8c444d132ab6e52735e
ksagar2411 <ksagar024@outlook.com>
2026-07-25 23:00:12 +0530

    sum of array

---
+++ src/arrays/ArraySumViaMethod.java
@@ -0,0 +1,44 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySumViaMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input the size of array: ");
+		int size = sc.nextInt();
+		
+		int[] arr = inputArray(size, sc);
+		printArray(arr);
+		System.out.println("The sum of array elements is:" + sumArray(arr));
+
+	}
+
+	public static int[] inputArray(int size, Scanner input) {
+		int[] initarr = new int[size];
+		System.out.println(Arrays.toString(initarr));
+		for (int i = 0; i < initarr.length; i++) {
+			System.out.print("Enter the value of element: " + i);
+			initarr[i] = input.nextInt();
+		}
+		return initarr;
+	}
+
+	public static void printArray(int[] arr) {
+
+		System.out.println("Array is: " + Arrays.toString(arr));
+	}
+
+	public static int sumArray(int[] arr) {
+		int sum = 0;
+		for (int j = 0; j < arr.length; j++) {
+			sum += arr[j];
+		}
+		return sum;
+
+	}
+
+}
--- src/arrays/SumOfArray.java
+++ src/arrays/SumOfArray.java
@@ -1,10 +1,27 @@
 package arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SumOfArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of array: ");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i=0; i<n;i++) {
+			System.out.println("Enter value for elemnt no. "+i+" :");
+			arr[i]= sc.nextInt(); 
+		}
+		sc.close();
+		int sum=0;
+		for(int j=0; j<n; j++) {
+			sum+= arr[j];
+		}
+		System.out.println("The sum of all array elements is:" + sum);
+		System.out.println("The array is" + Arrays.toString(arr));
 	}
-
 }
